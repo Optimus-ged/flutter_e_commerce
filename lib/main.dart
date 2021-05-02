ef52cee17242cf811bddebabd0b7ed2e1d6d4163
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-05-02 17:02:42 +0300

    Color class created for custom colors

--- lib/main.dart
+++ lib/main.dart
@@ -4,59 +4,20 @@ void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.black,
+        primarySwatch: Colors.grey,
+      ),
       home: MyApp(),
     ),
   );
 }
 
-// To remove the glow on the whole application, you can add it right under MaterialApp :
-// MaterialApp(
-//   builder: (context, child) {
-//     return ScrollConfiguration(
-//       behavior: MyBehavior(),
-//       child: child,
-//     );
-//   },
-//   home: new MyHomePage(),
-// );
-
-class MyBehavior extends ScrollBehavior {
-  @override
-  Widget buildViewportChrome(
-      BuildContext context, Widget child, AxisDirection axisDirection) {
-    return child;
-  }
-}
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Test listview"),
-      ),
-      body: ScrollConfiguration(
-        behavior: MyBehavior(),
-        child: ListView.builder(
-          itemCount: 100,
-          itemBuilder: (context, index) {
-            return Container(
-              alignment: Alignment.center,
-              padding: EdgeInsets.all(10),
-              color: Colors.black,
-              child: Text(
-                "$index",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                ),
-              ),
-            );
-          },
-        ),
-      ),
+    return Container(
+      
     );
   }
 }
-
-
---
+++ lib/utils/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+
+class CustomColors {
+  CustomColors._();
+  static const Color blueColor = Color(0xff142548);
+}
