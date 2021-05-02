8928f7cf0733c9234fc9c5600021c03b7bfff4a8
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-05-02 18:01:35 +0300

    The second part of login page created successfully

--- lib/pages/login_pages.dart
+++ lib/pages/login_pages.dart
@@ -12,22 +12,64 @@ class LoginPage extends StatelessWidget {
           child: Column(
             children: [
               Container(
-                color: Colors.grey,
+                // color: Colors.grey,
                 height: screen.height * .28,
                 width: double.infinity,
                 alignment: Alignment.bottomCenter,
                 child: Text(
                   "UZISHA",
                   style: TextStyle(
-                    fontSize: 30,
+                    fontSize: 28,
                     color: CustomColors.whiteColor,
                     letterSpacing: 5,
                   ),
                 ),
               ),
+              SizedBox(height: 60),
+              Container(
+                // color: Colors.black,
+                width: double.infinity,
+                alignment: Alignment.center,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    _builTitle(),
+                    _buildDivider(),
+                    _builTitle(),
+                  ],
+                ),
+              )
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  // Building text titles
+  Widget _builTitle() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Text("S'inscrire", style: _notActive()),
+    );
+  }
+
+  // Building textStyle
+  TextStyle _notActive() {
+    return TextStyle(
+      fontSize: 20,
+      color: CustomColors.greyColor,
+    );
+  }
+
+  // Build the divider between both textes
+  Widget _buildDivider() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: Container(
+        height: 25,
+        width: 0.2,
+        color: CustomColors.greyColor,
       ),
     );
   }
--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -4,4 +4,5 @@ class CustomColors {
   CustomColors._();
   static const Color blueColor = Color(0xff142548);
   static const Color whiteColor = Color(0xffFFFFFF);
+  static const Color greyColor = Color(0xff929498);
 }
