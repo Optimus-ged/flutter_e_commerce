5077ac467d49ea34d3aeb211fca4c0396ca980ad
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-05-03 11:04:21 +0300

    Sign up page initialized

---
+++ lib/pages/sign_up_page.dart
@@ -0,0 +1,10 @@
+import 'package:e_commerce/exports/all_exports.dart';
+
+class SignUpPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
