ef508a36da3d587fba2c26aee3c318222ffc5864
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-05-09 21:19:50 +0300

    Details page created successfully

--- lib/screens/pages/details_page.dart
+++ lib/screens/pages/details_page.dart
@@ -6,7 +6,6 @@ class DetailPage extends StatelessWidget {
     var screen = MediaQuery.of(context).size;
     final Map args = ModalRoute.of(context).settings.arguments;
     return Scaffold(
-      backgroundColor: AppTheme.lessPinkColor,
       body: Stack(
         children: [
           Positioned(
@@ -34,19 +33,91 @@ class DetailPage extends StatelessWidget {
                 ),
               ),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   SizedBox(height: 20),
-                  SizedBox(
-                    width: 100,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        _buildChoice(color: AppTheme.lessPinkColor),
-                        _buildChoice(color: AppTheme.yellowColor),
-                        _buildChoice(color: AppTheme.pinkColor)
-                      ],
+                  Padding(
+                    padding: EdgeInsets.only(left: 20, bottom: 10),
+                    child: Text(
+                      "Robe Coat",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white54,
+                        fontSize: 19,
+                      ),
                     ),
-                  )
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 20),
+                    child: SizedBox(
+                      width: 70,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          _buildChoice(
+                            color: AppTheme.lessPinkColor,
+                            isCliked: false,
+                          ),
+                          _buildChoice(
+                            color: AppTheme.yellowColor,
+                            isCliked: true,
+                          ),
+                          _buildChoice(
+                            color: AppTheme.pinkColor,
+                            isCliked: false,
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Container(
+                    padding: EdgeInsets.only(left: 20, right: 20),
+                    height: screen.height * .33,
+                    child: Text(
+                      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, reiciendis saepe ad perspiciatis cum iste illum molestiae minima voluptatem eos sint cupiditate eligendi accusamus perferendis velit eaque et! Provident, aut Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, reiciendis saepe ad perspiciatis cum iste illum molestiae minima voluptatem eos sint cupiditate eligendi accusamus perferendis velit eaque et! Provident, aut",
+                      style: TextStyle(
+                        color: Colors.white54,
+                        fontSize: 15,
+                        height: 1.3,
+                      ),
+                    ),
+                  ),
+                  Spacer(),
+                  Center(
+                    child: ClickAnimation(
+                      onTap: () => Navigator.of(context).pushNamed(Routes.home),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: AppTheme.pinkColor,
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        padding: EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 40,
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Icon(
+                              Icons.shopping_cart_outlined,
+                              color: AppTheme.whiteColor,
+                            ),
+                            SizedBox(width: 5),
+                            Text(
+                              "Ajouter au pannier",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 17,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20)
                 ],
               ),
             ),
@@ -78,13 +149,13 @@ class DetailPage extends StatelessWidget {
   }
 
   // Building circle container
-  _buildChoice({Color color}) {
+  _buildChoice({Color color, bool isCliked}) {
     return CircleAvatar(
-      backgroundColor: AppTheme.lessPinkColor,
-      radius: 20,
+      backgroundColor: isCliked ? AppTheme.whiteColor : Colors.transparent,
+      radius: 8,
       child: CircleAvatar(
         backgroundColor: color,
-        radius: 15,
+        radius: 7,
       ),
     );
   }
