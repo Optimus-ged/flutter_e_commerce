3f49380f751512d6b6aada3e8ebeee027452a1e5
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-05-13 13:54:16 +0200

    Bloc dependancy added successfully

--- lib/data/api/end_points.dart
+++ lib/data/api/end_points.dart
@@ -1,3 +1,13 @@
-class Endpoint{
-  
+class Endpoint {
+  const Endpoint();
+  // Key for validation access to endpoint
+  static const String key = "<<My key will be here>>";
+
+  // Initialization of base URLs
+  static const String baseUrl = "http://192.168.137.1:3000/api";
+
+  // Initialisation of all other endpoints
+  static const String allUsers = "$baseUrl/users/all";
+  static const String oneUser = "$baseUrl/users/one";
+  static const String allProducts = "$baseUrl/products/all";
 }
