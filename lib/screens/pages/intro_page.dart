3d2ffb55cddf5f2b5cc708dcc6465aed7ef70ddf
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-05-20 21:17:42 +0200

    Intro-page initialized successfully

---
+++ lib/screens/pages/intro_page.dart
@@ -0,0 +1,11 @@
+import 'package:e_commerce/exports/all_exports.dart';
+
+class IntroPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      top: false,
+      child: Container(),
+    );
+  }
+}
