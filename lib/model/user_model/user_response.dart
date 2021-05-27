e1dbac5e3ab7cb7dde6e060d3ac9458195ad726a
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-05-27 23:02:31 +0200

    An other repetition maded successfully on models creation

--- lib/model/user_model/user_response.dart
+++ lib/model/user_model/user_response.dart
@@ -26,3 +26,29 @@ class UserResponse {
     return data;
   }
 }
+
+class OneUser {
+  int status;
+  String message;
+  Users users;
+
+  OneUser({this.status, this.message, this.users});
+
+  OneUser.fromJson(Map<String, dynamic> json) {
+    status = json['satus'];
+    message = json['message'];
+    if (json['users'] != null) {
+      users = Users.fromJson(json['users']);
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final data = Map<String, dynamic>();
+    data['status'] = this.status;
+    data['message'] = this.message;
+    if (this.users != null) {
+      data['users'] = this.users.toJson();
+    }
+    return data;
+  }
+}
