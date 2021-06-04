283d039f3d673804f80b70ed5ab4f41c9db9bf3b
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-06-04 16:16:39 +0200

    Model for the local article created successfully

---
+++ lib/model/article_model/article_local.dart
@@ -0,0 +1,9 @@
+class ArticleLocal {
+  int id;
+  String photo;
+  String designation;
+  dynamic pu;
+  dynamic qte;
+
+  ArticleLocal({this.id, this.photo, this.designation, this.pu, this.qte});
+}
