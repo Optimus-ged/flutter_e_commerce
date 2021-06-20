47a2d0c054094ec85d7d99592fc57fc1c87e7be2
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-06-20 17:10:25 +0200

    Payment page improved successfully

--- lib/screens/pages/payment_page.dart
+++ lib/screens/pages/payment_page.dart
@@ -30,94 +30,83 @@ class _PaymentPageState extends State<PaymentPage> {
         builder: (context, snapshot) {
           if (snapshot.hasData) {
             browseList(data: snapshot.data);
-            return Stack(
-              children: [
-                Container(
-                  height: screen.height,
-                  width: screen.width,
-                  color: AppTheme.blueColor,
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: 50,
-                        width: double.infinity,
-                        child: Text("PANIER"),
+            return Container(
+              height: screen.height,
+              width: screen.width,
+              color: Colors.grey[50],
+              child: Column(
+                children: [
+                  SizedBox(height: 24),
+                  Container(
+                    child: Container(
+                      padding: EdgeInsets.symmetric(vertical: 15),
+                      child: Text(
+                        "PANIER",
+                        style: TextStyle(
+                          color: Colors.grey[300],
+                          fontWeight: FontWeight.w700,
+                          fontSize: 20,
+                        ),
                       ),
-                      Container(
+                    ),
+                  ),
+                  Expanded(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.vertical(
+                        top: Radius.circular(20),
+                      ),
+                      child: Container(
+                        width: screen.width,
                         decoration: BoxDecoration(
                           color: Colors.white,
                           borderRadius: BorderRadius.vertical(
-                            top: Radius.circular(30)
-                          )
+                            top: Radius.circular(20),
+                          ),
                         ),
-                        child: ListView.builder(
-                          itemCount: snapshot.data.length,
-                          // This make the list scrollinng only
-                          // when data are so many
-                          primary: false,
-                          itemBuilder: (context, index) {
-                            return _buildListItem(
-                                data: snapshot.data, index: index);
-                          },
+                        child: Column(
+                          children: [
+                            Expanded(
+                              child: ListView.builder(
+                                itemCount: snapshot.data.length,
+                                padding: EdgeInsets.only(top: 5),
+                                // This make the list scrollinng only
+                                // when data are so many
+                                primary: false,
+                                itemBuilder: (context, index) {
+                                  return _buildListItem(
+                                    data: snapshot.data,
+                                    index: index,
+                                  );
+                                },
+                              ),
+                            ),
+                            Text(
+                              "A payer : $totalPayment\$",
+                              style: TextStyle(
+                                color: Colors.grey[500],
+                                fontSize: 17,
+                                height: 1.5,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            CustomButton(
+                              title: "Effectuer payement",
+                              icon: Icon(
+                                Icons.payments,
+                                color: AppTheme.whiteColor,
+                              ),
+                              onTap: () {},
+                            ),
+                            SizedBox(height: 20)
+                          ],
                         ),
                       ),
-                      Text(
-                        "A payer : $totalPayment\$",
-                        style: TextStyle(
-                          color: Colors.grey[500],
-                          fontSize: 17,
-                          height: 1.5,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                      SizedBox(height: 10),
-                      CustomButton(
-                        title: "Effectuer payement",
-                        icon: Icon(
-                          Icons.payments,
-                          color: AppTheme.whiteColor,
-                        ),
-                        onTap: () {},
-                      ),
-                    ],
+                    ),
                   ),
-                )
-              ],
+                ],
+              ),
             );
-            // return Column(
-            //   children: [
-                // Expanded(
-                //   child: ListView.builder(
-                //     itemCount: snapshot.data.length,
-                //     // This make the list scrollinng only
-                //     // when data are so many
-                //     primary: false,
-                //     itemBuilder: (context, index) {
-                //       return _buildListItem(data: snapshot.data, index: index);
-                //     },
-                //   ),
-                // ),
-                // Text(
-                //   "A payer : $totalPayment\$",
-                //   style: TextStyle(
-                //     color: Colors.grey[500],
-                //     fontSize: 17,
-                //     height: 1.5,
-                //     fontWeight: FontWeight.w600,
-                //   ),
-                // ),
-                // SizedBox(height: 10),
-                // CustomButton(
-                //   title: "Effectuer payement",
-                //   icon: Icon(
-                //     Icons.payments,
-                //     color: AppTheme.whiteColor,
-                //   ),
-                //   onTap: () {},
-                // ),
-            //     SizedBox(height: 20)
-            //   ],
-            // );
           }
           return Center(
             child: CircularProgressIndicator(),
@@ -129,82 +118,84 @@ class _PaymentPageState extends State<PaymentPage> {
 
   // Building list view item
   Widget _buildListItem({List<LocalArticle> data, int index}) {
-    return ListTile(
-      minVerticalPadding: 0,
-      horizontalTitleGap: 5,
-      contentPadding: EdgeInsets.symmetric(vertical: 3, horizontal: 10),
-      leading: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [
-              AppTheme.radiantTopRight,
-              AppTheme.radiantTop,
-              AppTheme.radiantBotom
+    return Container(
+      child: ListTile(
+        minVerticalPadding: 0,
+        horizontalTitleGap: 5,
+        contentPadding: EdgeInsets.symmetric(vertical: 3, horizontal: 10),
+        leading: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                AppTheme.radiantTopRight,
+                AppTheme.radiantTop,
+                AppTheme.radiantBotom
+              ],
+            ),
+            borderRadius: BorderRadius.circular(6),
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(6),
+            child: CustomCashedImage(
+              imageUrl: "${Endpoint.uplaod}${data[index].photo}",
+              isPayPage: true,
+            ),
+          ),
+        ),
+        title: Container(
+          alignment: Alignment.topLeft,
+          height: 55,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "${data[index].designation}",
+              ),
+              Text(
+                "Pu : ${data[index].pu}\$\nPt : ${data[index].pu * data[index].qte}\$",
+                style: TextStyle(
+                  fontSize: 13,
+                  color: Colors.black54,
+                ),
+              ),
             ],
           ),
-          borderRadius: BorderRadius.circular(6),
         ),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(6),
-          child: CustomCashedImage(
-            imageUrl: "${Endpoint.uplaod}${data[index].photo}",
-            isPayPage: true,
+        trailing: Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.symmetric(vertical: 15),
+          height: 55,
+          width: 100,
+          child: Row(
+            children: [
+              _buildOptionItem(
+                title: "+",
+                ontap: () => _addOrDropData(
+                  data: data[index],
+                  index: index,
+                ),
+              ),
+              _buildOptionItem(
+                title: "${data[index].qte}",
+                flex: 1,
+                margin: 5,
+                color: Colors.transparent,
+                fontSize: 12,
+                isQte: true,
+              ),
+              _buildOptionItem(
+                title: "-",
+                ontap: () => _addOrDropData(
+                  data: data[index],
+                  index: index,
+                  isAdding: false,
+                ),
+              ),
+            ],
           ),
-        ),
-      ),
-      title: Container(
-        alignment: Alignment.topLeft,
-        height: 55,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "${data[index].designation}",
-            ),
-            Text(
-              "Pu : ${data[index].pu}\$\nPt : ${data[index].pu * data[index].qte}\$",
-              style: TextStyle(
-                fontSize: 13,
-                color: Colors.black54,
-              ),
-            ),
-          ],
-        ),
-      ),
-      trailing: Container(
-        alignment: Alignment.center,
-        padding: EdgeInsets.symmetric(vertical: 15),
-        height: 55,
-        width: 100,
-        child: Row(
-          children: [
-            _buildOptionItem(
-              title: "+",
-              ontap: () => _addOrDropData(
-                data: data[index],
-                index: index,
-              ),
-            ),
-            _buildOptionItem(
-              title: "${data[index].qte}",
-              flex: 1,
-              margin: 5,
-              color: Colors.transparent,
-              fontSize: 12,
-              isQte: true,
-            ),
-            _buildOptionItem(
-              title: "-",
-              ontap: () => _addOrDropData(
-                data: data[index],
-                index: index,
-                isAdding: false,
-              ),
-            ),
-          ],
         ),
       ),
     );
@@ -226,7 +217,7 @@ class _PaymentPageState extends State<PaymentPage> {
         child: Container(
           margin: EdgeInsets.symmetric(horizontal: margin),
           alignment: Alignment.center,
-          decoration: isQte
+          decoration: !isQte
               ? BoxDecoration(
                   color: color,
                   boxShadow: [
