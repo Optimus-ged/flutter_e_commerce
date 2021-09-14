99be8da06ceb98a0b8b35f74a0b3bf6a22bd72ca
Optimus-ged <gedeonyalakuhanda@gmail.com>
2021-09-14 15:28:03 +0200

    Floating action button added

--- lib/screens/pages/admin_homepage.dart
+++ lib/screens/pages/admin_homepage.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce/exports/all_exports.dart';
 import 'package:e_commerce/utils/app_theme.dart';
 import 'package:flutter/material.dart';
 
@@ -6,6 +7,12 @@ class AdminHomepage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        statusBarColor: AppTheme.whiteColor,
+        statusBarIconBrightness: Brightness.dark,
+      ),
+    );
     return SafeArea(
       top: false,
       child: Scaffold(
@@ -34,6 +41,11 @@ class AdminHomepage extends StatelessWidget {
             // _buildNavigation(),
           ],
         ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: AppTheme.blueColor,
+          child: Icon(Icons.add),
+          onPressed: () {},
+        ),
       ),
     );
   }
@@ -43,45 +55,81 @@ class BuildAdminListItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(bottom: 30),
+      padding: const EdgeInsets.only(bottom: 50),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Container(
             child: Row(
               children: [
-                Expanded(
-                  child: Container(
-                    height: 100,
-                    width: 100,
-                    margin: EdgeInsets.symmetric(horizontal: 2),
-                    color: Colors.amber,
-                  ),
-                ),
-                Expanded(
-                  child: Container(
-                    height: 100,
-                    width: 100,
-                    margin: EdgeInsets.symmetric(horizontal: 2),
-                    color: Colors.amber,
-                  ),
-                ),
-                Expanded(
-                  child: Container(
-                    height: 100,
-                    width: 100,
-                    margin: EdgeInsets.symmetric(horizontal: 2),
-                    color: Colors.amber,
-                  ),
-                )
+                buildItem(),
+                buildItem(),
+                buildItem(),
               ],
             ),
           ),
           SizedBox(height: 5),
-          Text('Designation'),
-          Text('pu'),
-          Text('details'),
+          Container(
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 5),
+                  child: Text(
+                    'Designation : ',
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.grey[700]),
+                  ),
+                ),
+                SizedBox(width: 10),
+                Text('designation')
+              ],
+            ),
+          ),
+          Container(
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 5),
+                  child: Text(
+                    'Pu : ',
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.grey[700]),
+                  ),
+                ),
+                SizedBox(width: 10),
+                Text('pu')
+              ],
+            ),
+          ),
+          Text(
+              'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni, ea? Consequatur, doloribus? Aut corrupti suscipit dolorem! Maxime atque deleniti rem commodi, incidunt assumenda, doloremque veniam laudantium nostrum tempora ullam consectetur.'),
         ],
+      ),
+    );
+  }
+
+  buildItem() {
+    return Expanded(
+      child: Container(
+        height: 150,
+        width: 100,
+        margin: EdgeInsets.symmetric(horizontal: 2),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              AppTheme.radiantTopRight,
+              AppTheme.radiantTop,
+              AppTheme.radiantBotom
+            ],
+          ),
+          borderRadius: BorderRadius.circular(10),
+        ),
       ),
     );
   }
