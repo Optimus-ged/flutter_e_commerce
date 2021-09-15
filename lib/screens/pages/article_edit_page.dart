f2988bb5a3e6560afe9afd90aa5a341badc0d0fe
Optimus-ged <gedeonyalakuhanda@gmail.com>
2021-09-15 12:19:48 +0200

    Article edit page initialized successfully

---
+++ lib/screens/pages/article_edit_page.dart
@@ -0,0 +1,17 @@
+import 'package:e_commerce/exports/all_exports.dart';
+
+class EditArticle extends StatefulWidget {
+  // const EditArticle({ Key? key }) : super(key: key);
+
+  @override
+  _EditArticleState createState() => _EditArticleState();
+}
+
+class _EditArticleState extends State<EditArticle> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
