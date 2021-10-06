52ad8ce997163a6615cda4f8f8d6777193a9f699
Optimus-ged <gedeonyalakuhanda@gmail.com>
2021-10-06 20:54:14 +0200

    Get all paiements event class created successfully

---
+++ lib/bloc/all_paiements_bloc/get_all_pyt_events.dart
@@ -0,0 +1,12 @@
+import 'package:e_commerce/exports/all_exports.dart';
+import 'package:equatable/equatable.dart';
+
+@immutable
+abstract class GetAllPaiementsEvent extends Equatable {
+  const GetAllPaiementsEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class LoadGetAllPaiements extends GetAllPaiementsEvent {}
