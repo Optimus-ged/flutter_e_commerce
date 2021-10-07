2e4281f9be2839fa8be8b2d61c7e535d899c54db
Optimus-ged <gedeonyalakuhanda@gmail.com>
2021-10-07 20:37:42 +0200

    Home page app bar created

--- lib/ngango_lib/pages/n_homepage.dart
+++ lib/ngango_lib/pages/n_homepage.dart
@@ -7,7 +7,41 @@ class NHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: SingleChildScrollView(),
+        child: Column(
+          children: [
+            Container(
+              child: Container(
+                // width: screen.width,
+                padding: EdgeInsets.only(top: 15, bottom: 15, left: 20),
+                color: Color(0xffAA000A),
+                child: Stack(
+                  children: [
+                    Positioned(
+                      left: 0,
+                       child: InkWell(
+                         onTap: () => Navigator.pop(context),
+                         child: Icon(
+                          Icons.arrow_back_ios,
+                          color: Colors.black45,
+                      ),
+                       ),
+                    ),
+                    Center(
+                      child: Text(
+                        "UTILISATEURS",
+                        style: TextStyle(
+                          color: Colors.grey[300],
+                          fontWeight: FontWeight.w700,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
