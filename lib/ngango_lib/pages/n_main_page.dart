4d53a92c6ef633ddb569f5d754d84fbc79878817
Optimus-ged <gedeonyalakuhanda@gmail.com>
2021-10-07 21:30:27 +0200

    Widget list created in order to display them by the botom tab bar

--- lib/ngango_lib/pages/n_main_page.dart
+++ lib/ngango_lib/pages/n_main_page.dart
@@ -1,17 +1,31 @@
+import 'package:e_commerce/ngango_lib/pages/n_agent_page.dart';
+import 'package:e_commerce/ngango_lib/pages/n_client_page.dart';
+import 'package:e_commerce/ngango_lib/pages/n_paiement_page.dart';
+import 'package:e_commerce/ngango_lib/pages/n_reservation_page.dart';
 import 'package:flutter/material.dart';
 
 class NMainPage extends StatefulWidget {
-  const NMainPage({ Key key }) : super(key: key);
+  const NMainPage({Key key}) : super(key: key);
 
   @override
   _NMainPageState createState() => _NMainPageState();
 }
 
 class _NMainPageState extends State<NMainPage> {
+  int currentTap = 0;
+
+  final List<Widget> screens = [
+    NAgentPage(),
+    NClientPage(),
+    NReservationPage(),
+    NPaiementPage()
+  ];
+
+  final pageStorageBucket bucket = PageStorageBucket();
+  Widget currentScreen = NAgentPage();
+  
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return Container();
   }
 }
