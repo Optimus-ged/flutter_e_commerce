2b594cc88a474aaadea6f7eabebda1f16d42b938
Optimus-ged <gedeonyalakuhanda@gmail.com>
2021-10-07 21:27:11 +0200

    Main page created

---
+++ lib/ngango_lib/pages/n_main_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class NMainPage extends StatefulWidget {
+  const NMainPage({ Key key }) : super(key: key);
+
+  @override
+  _NMainPageState createState() => _NMainPageState();
+}
+
+class _NMainPageState extends State<NMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
