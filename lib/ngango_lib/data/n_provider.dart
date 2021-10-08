9f9c0268dcdd43d40a129af64f2ca1e7ac599e97
Optimus-ged <gedeonyalakuhanda@gmail.com>
2021-10-08 21:17:01 +0200

    provider file created successfully

---
+++ lib/ngango_lib/data/n_provider.dart
@@ -0,0 +1,46 @@
+import 'package:dio/dio.dart';
+import 'package:e_commerce/optimus_lib/exports/all_exports.dart';
+import 'package:e_commerce/optimus_lib/model/paiement/create_paiement.dart';
+import 'package:e_commerce/optimus_lib/model/paiement/get_all_paiement.dart'
+    as all;
+import 'package:e_commerce/optimus_lib/model/user_model/signup_response.dart';
+
+class Provider {
+  Dio _dio;
+
+  // Initialisation of dio options
+  Provider() {
+    if (_dio == null) {
+      BaseOptions options = BaseOptions(
+        baseUrl: "${Endpoint.baseUrl}",
+        receiveDataWhenStatusError: true,
+        connectTimeout: 1000 * 30, // 30 seconds
+        receiveTimeout: 1000 * 30, // 30 seconds
+      );
+      _dio = Dio(options);
+      _dio.interceptors.add(AppInterceptors());
+    }
+  }
+
+  // Login
+  Future<LoginResponse> loginUser(String nom, String password) async {
+    try {
+      final result = await _dio.post(
+        "${Endpoint.loginUser}",
+        data: {"nom": nom, "mot_de_passe": password},
+        options: Options(
+          contentType: Headers.formUrlEncodedContentType,
+          headers: {
+            "Accept": "application/json",
+            // "App": "Admin",
+            // "key": Endpoint.key
+          },
+        ),
+      );
+      return LoginResponse.fromJson(result.data);
+    } catch (e) {
+      print("ERROR loginUser : ${e.toString()}");
+      throw e;
+    }
+  }
+}
